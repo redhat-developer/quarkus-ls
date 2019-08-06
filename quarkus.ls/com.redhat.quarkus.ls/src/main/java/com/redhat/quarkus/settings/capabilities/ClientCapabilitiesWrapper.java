89bf2b669949b90ef9cc6869afd77ec1651b5bb0
David Kwon <dakwon@redhat.com>
2019-08-06 14:38:32 -0400

    Manage client capabilities dynamically
    Signed-off-by: David Kwon <dakwon@redhat.com>

---
+++ quarkus.ls/com.redhat.quarkus.ls/src/main/java/com/redhat/quarkus/settings/capabilities/ClientCapabilitiesWrapper.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat Inc. and others. All rights reserved. This program
+ * and the accompanying materials which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors: Red Hat Inc. - initial API and implementation
+ *******************************************************************************/
+package com.redhat.quarkus.settings.capabilities;
+
+import org.eclipse.lsp4j.ClientCapabilities;
+import org.eclipse.lsp4j.DynamicRegistrationCapabilities;
+import org.eclipse.lsp4j.TextDocumentClientCapabilities;
+
+/**
+ * Determines if a client supports a specific capability dynamically
+ */
+public class ClientCapabilitiesWrapper {
+
+	private boolean v3Supported;
+	
+	private ClientCapabilities capabilities;
+
+	public ClientCapabilitiesWrapper() {
+		this(new ClientCapabilities());
+	}
+
+	public ClientCapabilitiesWrapper(ClientCapabilities capabilities) {
+		this.capabilities = capabilities;
+		this.v3Supported = capabilities != null ? capabilities.getTextDocument() != null : false;
+	}
+
+	/**
+	 * IMPORTANT
+	 * 
+	 * This should be up to date with all Server supported capabilities
+	 * 
+	 */
+
+	public boolean isCompletionDynamicRegistrationSupported() {
+		return v3Supported && isDynamicRegistrationSupported(getTextDocument().getCompletion());
+	}
+
+	public boolean isHoverDynamicRegistered() {
+		return v3Supported && isDynamicRegistrationSupported(getTextDocument().getHover());
+	}
+	
+	private boolean isDynamicRegistrationSupported(DynamicRegistrationCapabilities capability) {
+		return capability != null && capability.getDynamicRegistration() != null
+				&& capability.getDynamicRegistration().booleanValue();
+	}
+
+	public TextDocumentClientCapabilities getTextDocument() {
+		return this.capabilities.getTextDocument();
+	}
+}
---
+++ quarkus.ls/com.redhat.quarkus.ls/src/main/java/com/redhat/quarkus/settings/capabilities/ServerCapabilitiesInitializer.java
@@ -0,0 +1,47 @@
+/**
+ *  Copyright (c) 2018 Red Hat, Inc. and others.
+ *  All rights reserved. This program and the accompanying materials
+ *  are made available under the terms of the Eclipse Public License v2.0
+ *  which accompanies this distribution, and is available at
+ *  http://www.eclipse.org/legal/epl-v20.html
+ *
+ *  Contributors:
+ *  Red Hat Inc. - initial API and implementation
+ */
+
+package com.redhat.quarkus.settings.capabilities;
+
+import static com.redhat.quarkus.settings.capabilities.ServerCapabilitiesConstants.DEFAULT_COMPLETION_OPTIONS;
+
+import org.eclipse.lsp4j.ServerCapabilities;
+import org.eclipse.lsp4j.TextDocumentSyncKind;
+
+/**
+ * All default capabilities of this server
+ */
+public class ServerCapabilitiesInitializer {
+
+	private ServerCapabilitiesInitializer() {
+	}
+
+	/**
+	 * Returns all server capabilities (with default values) that aren't dynamic.
+	 * 
+	 * A service's dynamic capability is indicated by the client.
+	 * 
+	 * @param clientCapabilities
+	 * @return ServerCapabilities object
+	 */
+	public static ServerCapabilities getNonDynamicServerCapabilities(ClientCapabilitiesWrapper clientCapabilities) {
+		
+		ServerCapabilities serverCapabilities = new ServerCapabilities();
+		serverCapabilities.setTextDocumentSync(TextDocumentSyncKind.Incremental);
+		serverCapabilities.setHoverProvider(!clientCapabilities.isHoverDynamicRegistered());
+
+		if (!clientCapabilities.isCompletionDynamicRegistrationSupported()) {
+			serverCapabilities.setCompletionProvider(DEFAULT_COMPLETION_OPTIONS);
+		}
+		
+		return serverCapabilities;
+	}
+}
