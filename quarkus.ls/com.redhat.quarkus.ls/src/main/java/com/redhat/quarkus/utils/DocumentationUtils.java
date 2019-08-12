06a333b77934e08d365b184f92be90454394612c
azerr <azerr@redhat.com>
2019-08-12 11:17:13 +0200

    Finalize JDT LS Quarkus extension with ConfigRoot
    See #19
    
    Signed-off-by: azerr <azerr@redhat.com>

--- quarkus.ls/com.redhat.quarkus.ls/src/main/java/com/redhat/quarkus/utils/DocumentationUtils.java
+++ quarkus.ls/com.redhat.quarkus.ls/src/main/java/com/redhat/quarkus/utils/DocumentationUtils.java
@@ -21,55 +21,93 @@ import com.redhat.quarkus.commons.ExtendedConfigDescriptionBuildItem;
  */
 public class DocumentationUtils {
 
+	private DocumentationUtils() {
+
+	}
+
+	/**
+	 * Returns the documentation of the given Quarkus property.
+	 * 
+	 * @param item     the Quarkus property.
+	 * @param markdown true if documentation must be formatted as markdown and false
+	 *                 otherwise.
+	 * @return the documentation of the given Quarkus property.
+	 */
 	public static MarkupContent getDocumentation(ExtendedConfigDescriptionBuildItem item, boolean markdown) {
 		StringBuilder documentation = new StringBuilder();
+
+		// Title
+		if (markdown) {
+			documentation.append("**");
+		}
+		documentation.append(item.getPropertyName());
+		if (markdown) {
+			documentation.append("**");
+		}
+		documentation.append(System.lineSeparator());
+
+		// Javadoc
 		String javaDoc = item.getDocs();
 		if (javaDoc != null) {
+			documentation.append(System.lineSeparator());
 			documentation.append(javaDoc);
+			documentation.append(System.lineSeparator());
 		}
-		String type = item.getType();
-		if (type != null) {
-			if (documentation.length() > 0) {
-				documentation.append(System.lineSeparator());
+
+		// Type
+		addParameter("Type", item.getType(), documentation, markdown);
+
+		// Default value
+		addParameter("Default", item.getDefaultValue(), documentation, markdown);
+
+		// Config Phase
+		addParameter("Phase", getPhaseLabel(item.getPhase()), documentation, markdown);
+
+		// Location
+		String location = item.getLocation();
+		if (location != null) {
+			int slashIndex = location.lastIndexOf('/');
+			if (slashIndex != -1) {
+				location = location.substring(slashIndex + 1, location.length());
 			}
-			documentation.append("Type:");
-			if (markdown) {
-				documentation.append("`");
-			}
-			documentation.append(type);
-			if (markdown) {
-				documentation.append("`");
-			}
+			addParameter("Location", location, documentation, markdown);
 		}
-		String defaultValue = item.getDefaultValue();
-		if (defaultValue != null) {
-			if (documentation.length() > 0) {
-				documentation.append(System.lineSeparator());
-			}
-			documentation.append("Default:");
-			if (markdown) {
-				documentation.append("`");
-			}
-			documentation.append(defaultValue);
-			if (markdown) {
-				documentation.append("`");
-			}
-		}
-		String source = item.getSource();
-		if (source != null) {
-			if (documentation.length() > 0) {
-				documentation.append(System.lineSeparator());
-			}
-			documentation.append("Source:");
-			if (markdown) {
-				documentation.append("`");
-			}
-			documentation.append(source);
-			if (markdown) {
-				documentation.append("`");
-			}
-		}
+
+		// Source
+		addParameter("Source", item.getSource(), documentation, markdown);
+
 		return new MarkupContent(markdown ? MarkupKind.MARKDOWN : MarkupKind.PLAINTEXT, documentation.toString());
+	}
+
+	private static String getPhaseLabel(int phase) {
+		switch (phase) {
+		case ExtendedConfigDescriptionBuildItem.CONFIG_PHASE_BUILD_TIME:
+			return "buildtime";
+		case ExtendedConfigDescriptionBuildItem.CONFIG_PHASE_RUN_TIME:
+			return "runtime";
+		case ExtendedConfigDescriptionBuildItem.CONFIG_PHASE_BUILD_AND_RUN_TIME_FIXED:
+			return "buildtime & runtime";
+		default:
+			return "buildtime";
+		}
+	}
+
+	private static void addParameter(String name, String value, StringBuilder documentation, boolean markdown) {
+		if (value != null) {
+			documentation.append(System.lineSeparator());
+			if (markdown) {
+				documentation.append(" * ");
+			}
+			documentation.append(name);
+			documentation.append(": ");
+			if (markdown) {
+				documentation.append("`");
+			}
+			documentation.append(value);
+			if (markdown) {
+				documentation.append("`");
+			}
+		}
 	}
 
 }
