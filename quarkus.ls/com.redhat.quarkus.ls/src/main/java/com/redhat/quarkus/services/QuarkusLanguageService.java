8e627fba125487dddf44131d887a107d429a4e94
David Kwon <dakwon@redhat.com>
2019-08-12 16:20:26 -0400

    Implement hover for application.properties keys
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- quarkus.ls/com.redhat.quarkus.ls/src/main/java/com/redhat/quarkus/services/QuarkusLanguageService.java
+++ quarkus.ls/com.redhat.quarkus.ls/src/main/java/com/redhat/quarkus/services/QuarkusLanguageService.java
@@ -10,12 +10,14 @@
 package com.redhat.quarkus.services;
 
 import org.eclipse.lsp4j.CompletionList;
+import org.eclipse.lsp4j.Hover;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.jsonrpc.CancelChecker;
 
 import com.redhat.quarkus.commons.QuarkusProjectInfo;
 import com.redhat.quarkus.ls.commons.TextDocument;
 import com.redhat.quarkus.settings.QuarkusCompletionSettings;
+import com.redhat.quarkus.settings.QuarkusHoverSettings;
 
 /**
  * The Quarkus language service.
@@ -26,13 +28,20 @@ import com.redhat.quarkus.settings.QuarkusCompletionSettings;
 public class QuarkusLanguageService {
 
 	private final QuarkusCompletions completions;
+	private final QuarkusHover hover;
 
 	public QuarkusLanguageService() {
 		this.completions = new QuarkusCompletions();
+		this.hover = new QuarkusHover();
 	}
 
 	public CompletionList doComplete(TextDocument document, Position position, QuarkusProjectInfo projectInfo,
 			QuarkusCompletionSettings completionSettings, CancelChecker cancelChecker) {
 		return completions.doComplete(document, position, projectInfo, completionSettings, cancelChecker);
+	}
+
+	public Hover doHover(TextDocument document, Position position, QuarkusProjectInfo projectInfo,
+			QuarkusHoverSettings hoverSettings) {
+		return hover.doHover(document, position, projectInfo, hoverSettings);
 	}
 }
