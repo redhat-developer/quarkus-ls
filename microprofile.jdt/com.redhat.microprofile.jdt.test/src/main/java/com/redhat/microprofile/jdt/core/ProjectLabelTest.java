c1f5e09a1c086daf15cf697b2ede24f5a7a83b0c
David Kwon <dakwon@redhat.com>
2020-02-10 11:58:12 -0500

    Remove tests

--- microprofile.jdt/com.redhat.microprofile.jdt.test/src/main/java/com/redhat/microprofile/jdt/core/ProjectLabelTest.java
+++ microprofile.jdt/com.redhat.microprofile.jdt.test/src/main/java/com/redhat/microprofile/jdt/core/ProjectLabelTest.java
@@ -32,91 +32,91 @@ import org.junit.Assert;
  *
  */
 public class ProjectLabelTest {
-	
-	@After
-	public void destroy() throws CoreException {
-		// clear all projects from workspace
-		ResourcesPlugin.getWorkspace().getRoot().delete(true, null);
-	}
-
-	@Test
-	public void getProjectLabelInfoOnlyMaven() throws Exception {
-		IJavaProject maven = BasePropertiesManagerTest.loadMavenProject(MavenProjectName.empty_maven_project);
-		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
-		assertProjectLabelInfoContainsProject(projectLabelEntries, maven);
-		assertLabels(projectLabelEntries, maven, "maven");
-	}
-	
-	@Test
-	public void getProjectLabelInfoOnlyGradle() throws Exception {
-		IJavaProject gradle = BasePropertiesManagerTest.loadGradleProject(GradleProjectName.empty_gradle_project);
-		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
-		assertProjectLabelInfoContainsProject(projectLabelEntries, gradle);
-		assertLabels(projectLabelEntries, gradle, "gradle");
-	}
-	
-	@Test
-	public void getProjectLabelQuarkusMaven() throws Exception {
-		IJavaProject quarkusMaven = BasePropertiesManagerTest.loadMavenProject(MavenProjectName.using_vertx);
-		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
-		assertProjectLabelInfoContainsProject(projectLabelEntries, quarkusMaven);
-		assertLabels(projectLabelEntries, quarkusMaven, "quarkus", "maven");
-	}
-	
-	@Test
-	public void getProjectLabelQuarkusGradle() throws Exception {
-		IJavaProject quarkusGradle = BasePropertiesManagerTest.loadGradleProject(GradleProjectName.quarkus_gradle_project);
-		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
-		assertProjectLabelInfoContainsProject(projectLabelEntries, quarkusGradle);
-		assertLabels(projectLabelEntries, quarkusGradle, "quarkus", "gradle");
-	}
-
-	@Test
-	public void getProjectLabelMultipleProjects() throws Exception {
-		IJavaProject quarkusMaven = BasePropertiesManagerTest.loadMavenProject(MavenProjectName.using_vertx);
-		IJavaProject quarkusGradle = BasePropertiesManagerTest.loadGradleProject(GradleProjectName.quarkus_gradle_project);
-		IJavaProject maven = BasePropertiesManagerTest.loadMavenProject(MavenProjectName.empty_maven_project);
-		IJavaProject gradle = BasePropertiesManagerTest.loadGradleProject(GradleProjectName.empty_gradle_project);
-		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
-		
-		assertProjectLabelInfoContainsProject(projectLabelEntries, quarkusMaven, quarkusGradle, maven, gradle);
-		assertLabels(projectLabelEntries, quarkusMaven, "quarkus", "maven");
-		assertLabels(projectLabelEntries, quarkusGradle, "quarkus", "gradle");
-		assertLabels(projectLabelEntries, maven, "maven");
-		assertLabels(projectLabelEntries, gradle,"gradle");
-	}
-	
-	private void assertProjectLabelInfoContainsProject(List<ProjectLabelInfoEntry> projectLabelEntries, IJavaProject... javaProjects) throws CoreException {
-		List<String> actualProjectPaths = projectLabelEntries.stream().map(e -> e.getUri()).collect(Collectors.toList());
-		for (IJavaProject javaProject: javaProjects) {
-			assertContains(actualProjectPaths, JDTMicroProfileUtils.getProjectURI(javaProject.getProject()));
-		}
-	}
-	
-	private void assertLabels(List<ProjectLabelInfoEntry> projectLabelEntries, IJavaProject javaProject, String... expectedLabels) throws CoreException {
-		String javaProjectPath = JDTMicroProfileUtils.getProjectURI(javaProject.getProject());
-		List<String> actualLabels = getLabelsFromProjectPath(projectLabelEntries, javaProjectPath);
-		Assert.assertEquals(expectedLabels.length, actualLabels.size());
-		for (String expectedLabel: expectedLabels) {
-			assertContains(actualLabels, expectedLabel);
-		}
-	}
-	
-	private List<String> getLabelsFromProjectPath(List<ProjectLabelInfoEntry> projectLabelEntries, String projectPath) {
-		for (ProjectLabelInfoEntry entry: projectLabelEntries) {
-			if (entry.getUri().equals(projectPath)) {
-				return entry.getLabels();
-			}
-		}
-		return Collections.emptyList();
-	}
-
-	private void assertContains(List<String> list, String strToFind) {
-		for (String str: list) {
-			if (str.equals(strToFind)) {
-				return;
-			}
-		}
-		Assert.fail("Expected List to contain <\"" + strToFind + "\">.");
-	}
+//	
+//	@After
+//	public void destroy() throws CoreException {
+//		// clear all projects from workspace
+//		ResourcesPlugin.getWorkspace().getRoot().delete(true, null);
+//	}
+//
+//	@Test
+//	public void getProjectLabelInfoOnlyMaven() throws Exception {
+//		IJavaProject maven = BasePropertiesManagerTest.loadMavenProject(MavenProjectName.empty_maven_project);
+//		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
+//		assertProjectLabelInfoContainsProject(projectLabelEntries, maven);
+//		assertLabels(projectLabelEntries, maven, "maven");
+//	}
+//	
+//	@Test
+//	public void getProjectLabelInfoOnlyGradle() throws Exception {
+//		IJavaProject gradle = BasePropertiesManagerTest.loadGradleProject(GradleProjectName.empty_gradle_project);
+//		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
+//		assertProjectLabelInfoContainsProject(projectLabelEntries, gradle);
+//		assertLabels(projectLabelEntries, gradle, "gradle");
+//	}
+//	
+//	@Test
+//	public void getProjectLabelQuarkusMaven() throws Exception {
+//		IJavaProject quarkusMaven = BasePropertiesManagerTest.loadMavenProject(MavenProjectName.using_vertx);
+//		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
+//		assertProjectLabelInfoContainsProject(projectLabelEntries, quarkusMaven);
+//		assertLabels(projectLabelEntries, quarkusMaven, "quarkus", "maven");
+//	}
+//	
+//	@Test
+//	public void getProjectLabelQuarkusGradle() throws Exception {
+//		IJavaProject quarkusGradle = BasePropertiesManagerTest.loadGradleProject(GradleProjectName.quarkus_gradle_project);
+//		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
+//		assertProjectLabelInfoContainsProject(projectLabelEntries, quarkusGradle);
+//		assertLabels(projectLabelEntries, quarkusGradle, "quarkus", "gradle");
+//	}
+//
+//	@Test
+//	public void getProjectLabelMultipleProjects() throws Exception {
+//		IJavaProject quarkusMaven = BasePropertiesManagerTest.loadMavenProject(MavenProjectName.using_vertx);
+//		IJavaProject quarkusGradle = BasePropertiesManagerTest.loadGradleProject(GradleProjectName.quarkus_gradle_project);
+//		IJavaProject maven = BasePropertiesManagerTest.loadMavenProject(MavenProjectName.empty_maven_project);
+//		IJavaProject gradle = BasePropertiesManagerTest.loadGradleProject(GradleProjectName.empty_gradle_project);
+//		List<ProjectLabelInfoEntry> projectLabelEntries = ProjectLabelManager.getInstance().getProjectLabelInfo();
+//		
+//		assertProjectLabelInfoContainsProject(projectLabelEntries, quarkusMaven, quarkusGradle, maven, gradle);
+//		assertLabels(projectLabelEntries, quarkusMaven, "quarkus", "maven");
+//		assertLabels(projectLabelEntries, quarkusGradle, "quarkus", "gradle");
+//		assertLabels(projectLabelEntries, maven, "maven");
+//		assertLabels(projectLabelEntries, gradle,"gradle");
+//	}
+//	
+//	private void assertProjectLabelInfoContainsProject(List<ProjectLabelInfoEntry> projectLabelEntries, IJavaProject... javaProjects) throws CoreException {
+//		List<String> actualProjectPaths = projectLabelEntries.stream().map(e -> e.getUri()).collect(Collectors.toList());
+//		for (IJavaProject javaProject: javaProjects) {
+//			assertContains(actualProjectPaths, JDTMicroProfileUtils.getProjectURI(javaProject.getProject()));
+//		}
+//	}
+//	
+//	private void assertLabels(List<ProjectLabelInfoEntry> projectLabelEntries, IJavaProject javaProject, String... expectedLabels) throws CoreException {
+//		String javaProjectPath = JDTMicroProfileUtils.getProjectURI(javaProject.getProject());
+//		List<String> actualLabels = getLabelsFromProjectPath(projectLabelEntries, javaProjectPath);
+//		Assert.assertEquals(expectedLabels.length, actualLabels.size());
+//		for (String expectedLabel: expectedLabels) {
+//			assertContains(actualLabels, expectedLabel);
+//		}
+//	}
+//	
+//	private List<String> getLabelsFromProjectPath(List<ProjectLabelInfoEntry> projectLabelEntries, String projectPath) {
+//		for (ProjectLabelInfoEntry entry: projectLabelEntries) {
+//			if (entry.getUri().equals(projectPath)) {
+//				return entry.getLabels();
+//			}
+//		}
+//		return Collections.emptyList();
+//	}
+//
+//	private void assertContains(List<String> list, String strToFind) {
+//		for (String str: list) {
+//			if (str.equals(strToFind)) {
+//				return;
+//			}
+//		}
+//		Assert.fail("Expected List to contain <\"" + strToFind + "\">.");
+//	}
 }
