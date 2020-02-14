7241163dbdae5ab0d4a9e2a5854246da90072293
David Kwon <dakwon@redhat.com>
2020-02-14 14:34:54 -0500

    Change quarkus-gradle-project test project
    Signed-off-by: David Kwon <dakwon@redhat.com>

--- microprofile.jdt/com.redhat.microprofile.jdt.test/projects/gradle/quarkus-gradle-project/src/test/java/org/acme/GreetingResourceTest.java
+++ microprofile.jdt/com.redhat.microprofile.jdt.test/projects/gradle/quarkus-gradle-project/src/test/java/org/acme/GreetingResourceTest.java
@@ -7,7 +7,7 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class ExampleResourceTest {
+public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
