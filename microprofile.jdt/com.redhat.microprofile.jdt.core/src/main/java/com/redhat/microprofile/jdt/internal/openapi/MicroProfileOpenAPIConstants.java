e03bb1582401f8407ba70c217a01589a07a2f294
David Kwon <dakwon@redhat.com>
2020-03-17 10:54:22 -0400

    MicroProfile OpenAPI property support
    Signed-off-by: David Kwon <dakwon@redhat.com>

---
+++ microprofile.jdt/com.redhat.microprofile.jdt.core/src/main/java/com/redhat/microprofile/jdt/internal/openapi/MicroProfileOpenAPIConstants.java
@@ -0,0 +1,45 @@
+/*******************************************************************************
+* Copyright (c) 2020 Red Hat Inc. and others.
+* All rights reserved. This program and the accompanying materials
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v20.html
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*     Red Hat Inc. - initial API and implementation
+*******************************************************************************/
+package com.redhat.microprofile.jdt.internal.openapi;
+
+/**
+ * MicroProfile OpenAPI constants
+ * 
+ * @author David Kwon
+ *
+ */
+public class MicroProfileOpenAPIConstants {
+	
+	public static String OPEN_API_CONFIG = "org.eclipse.microprofile.openapi.OASConfig";
+	
+	public static String MODEL_READER = "mp.openapi.model.reader";
+	
+	public static String FILTER = "mp.openapi.filter";
+	
+	public static String SCAN_DISABLE = "mp.openapi.scan.disable";
+	
+	public static String SCAN_PACKAGES = "mp.openapi.scan.packages";
+	
+	public static String SCAN_CLASSES = "mp.openapi.scan.classes";
+	
+	public static String SCAN_EXCLUDE_PACKAGES = "mp.openapi.scan.exclude.packages";
+	
+	public static String SCAN_EXCLUDE_CLASSES = "mp.openapi.scan.exclude.classes";
+	
+	public static String SERVERS = "mp.openapi.servers";
+	
+	public static String SERVERS_PATH = "mp.openapi.servers.path.{*}";
+	
+	public static String SERVERS_OPERATION = "mp.openapi.servers.operation.{*}";
+	
+	public static String SCHEMA = "mp.openapi.schema.{*}";
+}
