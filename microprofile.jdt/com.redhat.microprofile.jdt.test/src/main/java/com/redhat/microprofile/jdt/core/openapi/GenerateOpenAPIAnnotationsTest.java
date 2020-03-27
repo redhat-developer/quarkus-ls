79acfbc64784c38662cea24491e87de0a1d66346
Benson Ning <bensonn@ca.ibm.com>
2020-03-27 12:55:41 -0400

    Remove extra packages

--- microprofile.jdt/com.redhat.microprofile.jdt.test/src/main/java/com/redhat/microprofile/jdt/core/openapi/GenerateOpenAPIAnnotationsTest.java
+++ microprofile.jdt/com.redhat.microprofile.jdt.test/src/main/java/com/redhat/microprofile/jdt/core/openapi/GenerateOpenAPIAnnotationsTest.java
@@ -16,18 +16,12 @@ import static com.redhat.microprofile.jdt.internal.core.java.MicroProfileForJava
 import static com.redhat.microprofile.jdt.internal.core.java.MicroProfileForJavaAssert.createCodeActionParams;
 import static com.redhat.microprofile.jdt.internal.core.java.MicroProfileForJavaAssert.te;
 
-import java.util.Collections;
-import java.util.logging.Logger;
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.lsp4j.CodeActionContext;
 import org.eclipse.lsp4j.Diagnostic;
 import org.eclipse.lsp4j.Position;
 import org.eclipse.lsp4j.Range;
-import org.eclipse.lsp4j.TextDocumentIdentifier;
-import org.eclipse.lsp4j.TextEdit;
 import org.junit.Test;
 
 import com.redhat.microprofile.commons.MicroProfileJavaCodeActionParams;
@@ -42,8 +36,6 @@ import com.redhat.microprofile.jdt.core.utils.IJDTUtils;
  */
 public class GenerateOpenAPIAnnotationsTest extends BasePropertiesManagerTest {
 	
-	private static final Logger LOGGER = Logger.getLogger(GenerateOpenAPIAnnotationsTest.class.getSimpleName());
-
 	@Test
 	public void GenerateOpenAPIAnnotationsAction() throws Exception {
 		IJavaProject javaProject = loadMavenProject(MavenProjectName.microprofile_openapi);
