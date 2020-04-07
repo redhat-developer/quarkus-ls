b77bc9a2b4ff4971abebd3b52ae11541b744bacc
David Kwon <dakwon@redhat.com>
2020-04-07 16:54:44 -0400

    MircoProfile LRA property support
    Signed-off-by: David Kwon <dakwon@redhat.com>

---
+++ microprofile.jdt/com.redhat.microprofile.jdt.core/src/main/java/com/redhat/microprofile/jdt/internal/lra/properties/MicroProfileLRAProvider.java
@@ -0,0 +1,41 @@
+/*******************************************************************************
+* Copyright (c) 2020 Red Hat Inc. and others.
+* All rights reserved. This program and the accompanying materials
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v20.html
+*
+* Contributors:
+*     Red Hat Inc. - initial API and implementation
+*******************************************************************************/
+package com.redhat.microprofile.jdt.internal.lra.properties;
+
+import static com.redhat.microprofile.jdt.internal.lra.MicroProfileLRAConstants.LRA_ANNOTATION;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jdt.core.IJavaProject;
+
+import com.redhat.microprofile.jdt.core.AbstractStaticPropertiesProvider;
+import com.redhat.microprofile.jdt.core.MicroProfileCorePlugin;
+import com.redhat.microprofile.jdt.core.SearchContext;
+import com.redhat.microprofile.jdt.core.utils.JDTTypeUtils;
+
+/**
+ * Properties provider that provides static MicroProfile LRA properties
+ * 
+ * @author David Kwon
+ * 
+ * @see https://github.com/eclipse/microprofile-lra/blob/2d7b24b4bcb755eadb19c74dadd504cc41b0c094/spec/src/main/asciidoc/microprofile-lra-spec.adoc#322-configuration-parameters
+ *
+ */
+public class MicroProfileLRAProvider extends AbstractStaticPropertiesProvider {
+	
+	public MicroProfileLRAProvider() {
+		super(MicroProfileCorePlugin.PLUGIN_ID, "/static-properties/mp-lra-metadata.json");
+	}
+
+	@Override
+	protected boolean isAdaptedFor(SearchContext context, IProgressMonitor monitor) {
+		IJavaProject javaProject = context.getJavaProject();
+		return (JDTTypeUtils.findType(javaProject, LRA_ANNOTATION) != null);
+	}
+}
--- microprofile.jdt/com.redhat.microprofile.jdt.test/src/main/java/com/redhat/microprofile/jdt/core/openapi/MicroProfileOpenAPITest.java
+++ microprofile.jdt/com.redhat.microprofile.jdt.test/src/main/java/com/redhat/microprofile/jdt/core/openapi/MicroProfileOpenAPITest.java
@@ -35,55 +35,55 @@ public class MicroProfileOpenAPITest extends BasePropertiesManagerTest {
 
 		assertProperties(infoFromClasspath,
 
-				p("microprofile-openapi", "mp.openapi.model.reader", "java.lang.String", 
+				p("microprofile-openapi-api", "mp.openapi.model.reader", "java.lang.String", 
 						"Configuration property to specify the fully qualified name of the OASModelReader implementation.",
 						true, null, null, null, 0, null),
 
-				p("microprofile-openapi", "mp.openapi.filter", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.filter", "java.lang.String",
 						"Configuration property to specify the fully qualified name of the OASFilter implementation.",
 						true, null, null, null, 0, null),
 				
-				p("microprofile-openapi", "mp.openapi.scan.disable", "boolean",
+				p("microprofile-openapi-api", "mp.openapi.scan.disable", "boolean",
 						"Configuration property to disable annotation scanning. Default value is `false`.",
 						true, null, null, null, 0, "false"),
 				
-				p("microprofile-openapi", "mp.openapi.scan.packages", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.scan.packages", "java.lang.String",
 						"Configuration property to specify the list of packages to scan.\r\n"
 						+ "For example, `mp.openapi.scan.packages=com.xyz.PackageA,com.xyz.PackageB`.",
 						true, null, null, null, 0, null),
 				
-				p("microprofile-openapi", "mp.openapi.scan.classes", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.scan.classes", "java.lang.String",
 						"Configuration property to specify the list of classes to scan.\r\n"
 						+ "For example, `mp.openapi.scan.classes=com.xyz.MyClassA,com.xyz.MyClassB`.",
 						true, null, null, null, 0, null),
 				
-				p("microprofile-openapi", "mp.openapi.scan.exclude.packages", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.scan.exclude.packages", "java.lang.String",
 						"Configuration property to specify the list of packages to exclude from scans.\r\n"
 						+ "For example, `mp.openapi.scan.exclude.packages=com.xyz.PackageC,com.xyz.PackageD`.",
 						true, null, null, null, 0, null),
 				
-				p("microprofile-openapi", "mp.openapi.scan.exclude.classes", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.scan.exclude.classes", "java.lang.String",
 						"Configuration property to specify the list of classes to exclude from scans.\r\n"
 						+ "For example, `mp.openapi.scan.exclude.classes=com.xyz.MyClassC,com.xyz.MyClassD`.",
 						true, null, null, null, 0, null),
 				
-				p("microprofile-openapi", "mp.openapi.servers", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.servers", "java.lang.String",
 						"Configuration property to specify the list of global servers that provide connectivity information.\r\n"
 						+ "For example, `mp.openapi.servers=https://xyz.com/v1,https://abc.com/v1`.",
 						true, null, null, null, 0, null),
 				
-				p("microprofile-openapi", "mp.openapi.servers.path.{*}", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.servers.path.{*}", "java.lang.String",
 						"Prefix of the configuration property to specify an alternative list of servers to service all operations in a path."
 						+ "For example, `mp.openapi.servers.path./airlines/bookings/{id}=https://xyz.io/v1`.",
 						true, null, null, null, 0, null),
 				
-				p("microprofile-openapi", "mp.openapi.servers.operation.{*}", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.servers.operation.{*}", "java.lang.String",
 						"Prefix of the configuration property to specify an alternative list of servers to service an operation."
 						+ "Operations that want to specify an alternative list of servers must define an `operationId`, a unique string used to identify the operation."
 						+ "For example, `mp.openapi.servers.operation.getBooking=https://abc.io/v1`.",
 						true, null, null, null, 0, null),
 				
-				p("microprofile-openapi", "mp.openapi.schema.{*}", "java.lang.String",
+				p("microprofile-openapi-api", "mp.openapi.schema.{*}", "java.lang.String",
 						"Prefix of the configuration property to specify a schema for a specific class, in JSON format."
 								+ "The remainder of the property key must be the fully-qualified class name."
 								+ "The value must be a valid OpenAPI schema object, specified in the JSON format."
