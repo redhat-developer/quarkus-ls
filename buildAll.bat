aaf1d532bb71cd002358130174abe278559058f9
Fred Bricon <fbricon@gmail.com>
2020-07-11 00:37:20 +0200

    Delete microprofile projects
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- buildAll.bat
+++ buildAll.bat
@@ -1,5 +1,2 @@
-cd microprofile.jdt && .\mvnw.cmd clean install ^
-&& cd ..\quarkus.jdt.ext && .\mvnw.cmd clean verify ^
-&& cd ..\microprofile.ls\org.eclipse.lsp4mp.ls && .\mvnw.cmd clean install ^
-&& cd ..\..\quarkus.ls.ext\com.redhat.quarkus.ls && .\mvnw.cmd clean verify ^
-&& cd ..\..
+cd quarkus.jdt.ext && .\mvnw.cmd clean verify && cd .. ^
+cd quarkus.ls.ext\com.redhat.quarkus.ls && .\mvnw.cmd clean verify && cd ..\..
