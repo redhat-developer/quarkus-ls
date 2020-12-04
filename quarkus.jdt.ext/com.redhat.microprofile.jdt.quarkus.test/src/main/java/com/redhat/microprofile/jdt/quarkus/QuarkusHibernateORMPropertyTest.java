fc356e8643275bc2ea579791a73d9caa595e1598
Roland Grunberg <rgrunber@redhat.com>
2020-12-04 11:32:27 -0500

    quarkus.hibernate-orm.database.generation values should be enumerated.
    - Fixes redhat-developer/vscode-quarkus#317
    - Add testcase
    
    Signed-off-by: Roland Grunberg <rgrunber@redhat.com>

---
+++ quarkus.jdt.ext/com.redhat.microprofile.jdt.quarkus.test/src/main/java/com/redhat/microprofile/jdt/quarkus/QuarkusHibernateORMPropertyTest.java
@@ -0,0 +1,49 @@
+/*******************************************************************************
+* Copyright (c) 2020 Red Hat Inc. and others.
+* All rights reserved. This program and the accompanying materials
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v20.html
+*
+* Contributors:
+*     Red Hat Inc. - initial API and implementation
+*******************************************************************************/
+package com.redhat.microprofile.jdt.quarkus;
+
+
+import static org.eclipse.lsp4mp.jdt.internal.core.MicroProfileAssert.assertProperties;
+import static org.eclipse.lsp4mp.jdt.internal.core.MicroProfileAssert.assertPropertiesDuplicate;
+import static org.eclipse.lsp4mp.jdt.internal.core.MicroProfileAssert.p;
+
+import org.eclipse.lsp4mp.commons.MicroProfileProjectInfo;
+import org.eclipse.lsp4mp.commons.MicroProfilePropertiesScope;
+import org.eclipse.lsp4mp.jdt.core.BasePropertiesManagerTest;
+import org.junit.Test;
+
+
+/**
+ * Test the availability of the Quarkus Hibernate ORM properties
+ * 
+ * @author Angelo ZERR
+ *
+ */
+public class QuarkusHibernateORMPropertyTest extends BasePropertiesManagerTest {
+
+	@Test
+	public void quarkusContainerImages() throws Exception {
+
+		MicroProfileProjectInfo infoFromClasspath = getMicroProfileProjectInfoFromMavenProject(
+				MavenProjectName.hibernate_orm_resteasy, MicroProfilePropertiesScope.SOURCES_AND_DEPENDENCIES);
+
+		assertProperties(infoFromClasspath,
+
+				p("quarkus-hibernate-orm", "quarkus.hibernate-orm.database.generation", "java.lang.String",
+						"Select whether the database schema is generated or not."
+						+ "\n\n`drop-and-create` is awesome in development mode."
+						+ "\n\nAccepted values: `none`, `create`, `drop-and-create`, `drop`, `update`.", true,
+						"io.quarkus.hibernate.orm.deployment.HibernateOrmConfig.HibernateOrmConfigDatabase", "generation", null, 1,
+						"none"));
+
+		assertPropertiesDuplicate(infoFromClasspath);
+	}
+
+}
