ed2b933955762a26390e931f87f50e8557580605
Fred Bricon <fbricon@gmail.com>
2021-11-29 12:26:29 +0100

    Fix buildAll.sh, again
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- buildAll.sh
+++ buildAll.sh
@@ -1,4 +1,4 @@
-cd quarkus.jdt.ext && ./mvnw clean verify  && cd .. && \
-cd quarkus.ls.ext/com.redhat.quarkus.ls && ./mvnw clean verify && cd .. \
-cd qute.jdt && ./mvnw clean verify  && cd .. && \
+cd quarkus.jdt.ext && ./mvnw clean verify && cd .. \
+cd quarkus.ls.ext/com.redhat.quarkus.ls && ./mvnw clean verify && cd ../.. \
+cd qute.jdt && ./mvnw clean verify && cd .. \
 cd qute.ls/com.redhat.qute.ls && ./mvnw clean verify && cd ../.. 
