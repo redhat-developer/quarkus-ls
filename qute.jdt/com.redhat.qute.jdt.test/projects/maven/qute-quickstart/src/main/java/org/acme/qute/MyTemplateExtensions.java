ff56ef1b7d7d62d8397c8778f3881b74a0de0aaa
David Thompson <davthomp@redhat.com>
2022-07-28 13:28:40 -0400

    Insert newly generated members at end
    For the "generate missing member" qute-ls CodeAction,
    generate the missing member at the end of the respective list.
    i.e. place the new field at the end of the list of fields,
    or place the new getter method at the end of the list of methods.
    
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- qute.jdt/com.redhat.qute.jdt.test/projects/maven/qute-quickstart/src/main/java/org/acme/qute/MyTemplateExtensions.java
+++ qute.jdt/com.redhat.qute.jdt.test/projects/maven/qute-quickstart/src/main/java/org/acme/qute/MyTemplateExtensions.java
@@ -5,4 +5,8 @@ import io.quarkus.qute.TemplateExtension;
 @TemplateExtension
 public class MyTemplateExtensions {
 
+	public static String lengthMinusTwo(String str) {
+		return Integer.toString(str.length() - 2);
+	}
+
 }
