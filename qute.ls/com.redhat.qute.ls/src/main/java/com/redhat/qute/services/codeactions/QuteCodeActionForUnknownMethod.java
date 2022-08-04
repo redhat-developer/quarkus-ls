43c088c21e4e3e7eae9a12d9b6b80d3302bd33c9
Alexander Chen <alchen@redhat.com>
2022-08-04 12:24:36 -0400

    Added Qute CodeAction(s) for similar text suggestions for UnknownMethod
    Signed-off-by: Alexander Chen <alchen@redhat.com>

--- qute.ls/com.redhat.qute.ls/src/main/java/com/redhat/qute/services/codeactions/QuteCodeActionForUnknownMethod.java
+++ qute.ls/com.redhat.qute.ls/src/main/java/com/redhat/qute/services/codeactions/QuteCodeActionForUnknownMethod.java
@@ -11,10 +11,13 @@
 *******************************************************************************/
 package com.redhat.qute.services.codeactions;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
 import org.eclipse.lsp4j.CodeAction;
 import org.eclipse.lsp4j.Diagnostic;
@@ -25,11 +28,15 @@ import com.redhat.qute.parser.expression.MethodPart;
 import com.redhat.qute.parser.template.Node;
 import com.redhat.qute.parser.template.Template;
 import com.redhat.qute.project.datamodel.JavaDataModelCache;
+import com.redhat.qute.project.datamodel.resolvers.MethodValueResolver;
 import com.redhat.qute.services.diagnostics.QuteErrorCode;
+import com.redhat.qute.services.nativemode.JavaTypeAccessibiltyRule;
+import com.redhat.qute.services.nativemode.JavaTypeFilter;
+import com.redhat.qute.settings.QuteNativeSettings;
 
 /**
  * Code actions for {@link QuteErrorCode#UnknownMethod}.
- * 
+ *
  * @author Angelo ZERR
  *
  */
@@ -49,23 +56,109 @@ public class QuteCodeActionForUnknownMethod extends AbstractQuteCodeAction {
 			if (node == null) {
 				return;
 			}
-			
+
 			ResolvedJavaTypeInfo baseResolvedType = request.getJavaTypeOfCoveredNode(javaCache);
 			if (baseResolvedType == null) {
 				return;
 			}
-			
+
 			MethodPart part = (MethodPart) node;
 			Template template = request.getTemplate();
 			Diagnostic diagnostic = request.getDiagnostic();
 
-			
-			// TODO : implement code actions
+			QuteNativeSettings nativeImageSettings = request.getSharedSettings().getNativeSettings();
+
+			doCodeActionsForSimilarValues(part, template, diagnostic, baseResolvedType, nativeImageSettings, codeActions);
 
 		} catch (BadLocationException e) {
 			LOGGER.log(Level.SEVERE, "Creation of unknown method code action failed", e);
 		}
 
+	}
+
+	/**
+	 * Create CodeAction(s) for similar text suggestions for methods
+	 *
+	 * @param part             the method part
+	 * @param template         the Qute template
+	 * @param diagnostic       the UndefinedVariable diagnostic
+	 * @param baseResolvedType the resolved Java type info
+	 * @param codeActions      list of CodeActions
+	 *
+	 * @throws BadLocationException
+	 */
+	private void doCodeActionsForSimilarValues(MethodPart part, Template template, Diagnostic diagnostic,
+			ResolvedJavaTypeInfo baseResolvedType, QuteNativeSettings nativeImageSettings, List<CodeAction> codeActions) throws BadLocationException {
+		Collection<String> availableValues = collectAvailableValuesForMethodPart(part, template, baseResolvedType, nativeImageSettings);
+		doCodeActionsForSimilarValues(part, availableValues, template, diagnostic, codeActions);
+	}
+
+	/**
+	 * Collect similar text suggestions for methods
+	 *
+	 * @param part             the method part
+	 * @param template         the Qute template
+	 * @param baseResolvedType the resolved Java type info
+	 * @param nativeImageSettings   the Qute native settings
+	 *
+	 * @return a list of available methods for the resolved type
+	 */
+	private Collection<String> collectAvailableValuesForMethodPart(MethodPart part, Template template,
+			ResolvedJavaTypeInfo baseResolvedType, QuteNativeSettings nativeImageSettings) {
+		Collection<String> availableValues = new HashSet<>();
+		String projectUri = template.getProjectUri();
+
+		JavaTypeFilter filter = javaCache.getJavaTypeFilter(projectUri, nativeImageSettings);
+
+		// Resolve methods defined in Java type
+		Collection<String> availableMethodNames = collectJavaTypeMethodNames(part, template, projectUri, baseResolvedType, filter, nativeImageSettings);
+		for (String methodName : availableMethodNames) {
+			availableValues.add(methodName);
+		}
+
+		// Resolve methods defined in built-in Qute value resolvers
+		List<MethodValueResolver> resolvers = javaCache.getResolversFor(baseResolvedType, template.getProjectUri());
+		for (MethodValueResolver method : resolvers) {
+			if (method.isValidName()) {
+				availableValues.add(method.getMethodName());
+			}
+		}
+
+		return availableValues;
+	}
+
+	private Collection<String> collectJavaTypeMethodNames(MethodPart part, Template template, String projectUri,
+	ResolvedJavaTypeInfo baseResolvedType, JavaTypeFilter filter, QuteNativeSettings nativeImageSettings) {
+		Collection<String> availableMethodNames = new HashSet<>();
+
+		List<String> javaMethodNames = baseResolvedType.getMethods().stream().map(x -> x.getMethodName())
+				.collect(Collectors.toList());
+		for (String methodName : javaMethodNames) {
+			availableMethodNames.add(methodName);
+		}
+
+		JavaTypeAccessibiltyRule javaTypeAccessibility = filter.getJavaTypeAccessibility(baseResolvedType,
+				template.getJavaTypesSupportedInNativeMode());
+
+		if (!isIgnoreSuperclasses(baseResolvedType, javaTypeAccessibility, filter)) {
+			List<String> extendedTypes = baseResolvedType.getExtendedTypes();
+			if (extendedTypes != null) {
+				for (String extendedType : extendedTypes) {
+					ResolvedJavaTypeInfo resolvedExtendedType = javaCache.resolveJavaType(extendedType, projectUri)
+							.getNow(null);
+					if (resolvedExtendedType != null) {
+						availableMethodNames.addAll(collectJavaTypeMethodNames(part, template, projectUri, resolvedExtendedType, filter, nativeImageSettings));
+					}
+				}
+			}
+		}
+
+		return availableMethodNames;
+	}
+
+	private static boolean isIgnoreSuperclasses(ResolvedJavaTypeInfo baseType,
+	JavaTypeAccessibiltyRule javaTypeAccessibility, JavaTypeFilter filter) {
+		return filter != null && javaTypeAccessibility != null && filter.isIgnoreSuperclasses(baseType, javaTypeAccessibility);
 	}
 
 }
