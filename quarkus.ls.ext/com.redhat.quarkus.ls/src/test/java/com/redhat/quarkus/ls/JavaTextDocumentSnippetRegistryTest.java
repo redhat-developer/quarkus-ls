1d06685d099238d8527cad0ff5079de4c0214209
David Thompson <davthomp@redhat.com>
2023-01-23 10:58:49 -0500

    Update jdt.ls to 1.20.0, fix tests for lsp4mp 0.7.0
    Signed-off-by: David Thompson <davthomp@redhat.com>

--- quarkus.ls.ext/com.redhat.quarkus.ls/src/test/java/com/redhat/quarkus/ls/JavaTextDocumentSnippetRegistryTest.java
+++ quarkus.ls.ext/com.redhat.quarkus.ls/src/test/java/com/redhat/quarkus/ls/JavaTextDocumentSnippetRegistryTest.java
@@ -23,6 +23,7 @@ import org.eclipse.lsp4mp.commons.ProjectLabelInfoEntry;
 import org.eclipse.lsp4mp.ls.commons.snippets.ISnippetContext;
 import org.eclipse.lsp4mp.ls.commons.snippets.Snippet;
 import org.eclipse.lsp4mp.ls.commons.snippets.SnippetRegistry;
+import org.eclipse.lsp4mp.snippets.JavaSnippetCompletionContext;
 import org.eclipse.lsp4mp.snippets.SnippetContextForJava;
 
 /**
@@ -33,32 +34,35 @@ import org.eclipse.lsp4mp.snippets.SnippetContextForJava;
  */
 public class JavaTextDocumentSnippetRegistryTest {
 
-  @Test
-  public void javaSnippets() {
-    JavaTextDocumentSnippetRegistry registry = new JavaTextDocumentSnippetRegistry();
-    Assert.assertFalse("Tests has Quarkus Java snippets", registry.getSnippets().isEmpty());
+	@Test
+	public void javaSnippets() {
+		JavaTextDocumentSnippetRegistry registry = new JavaTextDocumentSnippetRegistry();
+		Assert.assertFalse("Tests has Quarkus Java snippets", registry.getSnippets().isEmpty());
 
-    Optional<Snippet> qrtcSnippet = findByPrefix("qtrc", registry);
-    Assert.assertTrue("Tests has Quarkus - new test resource class (qtrc) snippets", qrtcSnippet.isPresent());
-    
-    Optional<Snippet> qitrcSnippet = findByPrefix("qitrc", registry);
-    Assert.assertTrue("Tests has Quarkus - new integration test resource class (qitrc) snippet", qrtcSnippet.isPresent());
+		Optional<Snippet> qrtcSnippet = findByPrefix("qtrc", registry);
+		Assert.assertTrue("Tests has Quarkus - new test resource class (qtrc) snippets", qrtcSnippet.isPresent());
 
-    ISnippetContext<?> context = qrtcSnippet.get().getContext();
-    Assert.assertNotNull("Quarkus - new test resource class (qtrc) snippet has context", context);
+		Optional<Snippet> qitrcSnippet = findByPrefix("qitrc", registry);
+		Assert.assertTrue("Tests has Quarkus - new integration test resource class (qitrc) snippet",
+				qitrcSnippet.isPresent());
 
-    Assert.assertTrue("Quarkus - new test resource class (qtrc) snippet context is Java context", context instanceof SnippetContextForJava);
+		ISnippetContext<?> context = qrtcSnippet.get().getContext();
+		Assert.assertNotNull("Quarkus - new test resource class (qtrc) snippet has context", context);
 
-    ProjectLabelInfoEntry projectInfo = new ProjectLabelInfoEntry("", "", new ArrayList<>());
-    boolean match = ((SnippetContextForJava) context).isMatch(projectInfo);
-    Assert.assertFalse("Project has no io.quarkus.test.junit.QuarkusTest type", match);
+		Assert.assertTrue("Quarkus - new test resource class (qtrc) snippet context is Java context",
+				context instanceof SnippetContextForJava);
 
-    ProjectLabelInfoEntry projectInfo2 = new ProjectLabelInfoEntry("", "", Arrays.asList("io.quarkus.test.junit.QuarkusTest"));
-    boolean match2 = ((SnippetContextForJava) context).isMatch(projectInfo2);
-    Assert.assertTrue("Project has io.quarkus.test.junit.QuarkusTest type", match2);
-  }
+		ProjectLabelInfoEntry projectInfo = new ProjectLabelInfoEntry("", "", new ArrayList<>());
+		boolean match = ((SnippetContextForJava) context).isMatch(new JavaSnippetCompletionContext(projectInfo, null));
+		Assert.assertFalse("Project has no io.quarkus.test.junit.QuarkusTest type", match);
 
-  private static Optional<Snippet> findByPrefix(String prefix, SnippetRegistry registry) {
-    return registry.getSnippets().stream().filter(snippet -> snippet.getPrefixes().contains(prefix)).findFirst();
-  }
+		ProjectLabelInfoEntry projectInfo2 = new ProjectLabelInfoEntry("", "",
+				Arrays.asList("io.quarkus.test.junit.QuarkusTest"));
+		boolean match2 = ((SnippetContextForJava) context).isMatch(new JavaSnippetCompletionContext(projectInfo2, null));
+		Assert.assertTrue("Project has io.quarkus.test.junit.QuarkusTest type", match2);
+	}
+
+	private static Optional<Snippet> findByPrefix(String prefix, SnippetRegistry registry) {
+		return registry.getSnippets().stream().filter(snippet -> snippet.getPrefixes().contains(prefix)).findFirst();
+	}
 }
