6ea798ee2b8cebf9d2a5cd609d5be2721cec4077
Fred Bricon <fbricon@gmail.com>
2024-01-26 18:54:19 +0100

    fix: use newer JDT.LS's JavadocContentAccess2 methods
    Signed-off-by: Fred Bricon <fbricon@gmail.com>

--- qute.jdt/com.redhat.qute.jdt/src/main/java/com/redhat/qute/jdt/internal/ls/JDTUtilsLSImpl.java
+++ qute.jdt/com.redhat.qute.jdt/src/main/java/com/redhat/qute/jdt/internal/ls/JDTUtilsLSImpl.java
@@ -31,7 +31,6 @@ import org.eclipse.jdt.ls.core.internal.JavaLanguageServerPlugin;
 import org.eclipse.jdt.ls.core.internal.ResourceUtils;
 import org.eclipse.jdt.ls.core.internal.handlers.DocumentLifeCycleHandler;
 import org.eclipse.jdt.ls.core.internal.handlers.JsonRpcHelpers;
-import org.eclipse.jdt.ls.core.internal.javadoc.JavadocContentAccess;
 import org.eclipse.jdt.ls.core.internal.javadoc.JavadocContentAccess2;
 import org.eclipse.lsp4j.Location;
 import org.eclipse.lsp4j.Range;
@@ -116,7 +115,7 @@ public class JDTUtilsLSImpl implements IJDTUtils {
 	public String getJavadoc(IMember member, DocumentFormat documentFormat) throws JavaModelException {
 		boolean markdown = DocumentFormat.Markdown.equals(documentFormat);
 		Reader reader = markdown ? JavadocContentAccess2.getMarkdownContentReader(member)
-				: JavadocContentAccess.getPlainTextContentReader(member);
+				: JavadocContentAccess2.getPlainTextContentReader(member);
 		return reader != null ? toString(reader) : null;
 	}
 
