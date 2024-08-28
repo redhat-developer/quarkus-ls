bd201b20d64ea5466c5b3b5617c49a2f34dd8b6e
azerr <azerr@redhat.com>
2024-08-28 17:18:10 +0200

    Version 18 release duplicate key issue
    Fixes https://github.com/redhat-developer/vscode-quarkus/issues/822
    
    Signed-off-by: azerr <azerr@redhat.com>

--- qute.jdt/com.redhat.qute.jdt/src/main/java/com/redhat/qute/jdt/internal/template/datamodel/CheckedTemplateSupport.java
+++ qute.jdt/com.redhat.qute.jdt/src/main/java/com/redhat/qute/jdt/internal/template/datamodel/CheckedTemplateSupport.java
@@ -277,7 +277,7 @@ public class CheckedTemplateSupport extends AbstractAnnotationTypeReferenceDataM
 					if (templateOrFragment.getParameter(parameter.getKey()) == null) {
 						// Add parameter if it doesn't exist
 						// to avoid parameters duplication
-						templateOrFragment.getParameters().add(parameter);
+						templateOrFragment.addParameter(parameter);
 					}
 				}
 			}
