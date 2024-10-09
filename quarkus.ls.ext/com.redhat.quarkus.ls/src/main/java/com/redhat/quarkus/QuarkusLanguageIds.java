a9f5a3abf2cde2cfb21ac0b95fbc0046f2cdcfa2
azerr <azerr@redhat.com>
2024-10-09 11:46:47 +0200

    fix: add languageId filter for options similar to microprofile
    Signed-off-by: azerr <azerr@redhat.com>

---
+++ quarkus.ls.ext/com.redhat.quarkus.ls/src/main/java/com/redhat/quarkus/QuarkusLanguageIds.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+* Copyright (c) 2024 Red Hat Inc. and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v. 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+* which is available at https://www.apache.org/licenses/LICENSE-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+*
+* Contributors:
+*     Red Hat Inc. - initial API and implementation
+*******************************************************************************/
+package com.redhat.quarkus;
+
+/**
+ * Quarkus language ids.
+ */
+public class QuarkusLanguageIds {
+
+	public static final String QUARKUS_PROPERTIES = "quarkus-properties";
+
+	private QuarkusLanguageIds() {
+	}
+}
